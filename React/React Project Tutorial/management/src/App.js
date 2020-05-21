682c6147048b48c1a0b44206b21765293f172292
Jang Jae Yong <jangjs2412@gmail.com>
2020-05-21 12:40:45 +0900

    Create Customer.js

--- React/React Project Tutorial/management/src/App.js
+++ React/React Project Tutorial/management/src/App.js
@@ -1,14 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-//import { render } from '@testing-library/react';
+import Customer from './components/Customer';
+
+const customers = [
+  {
+    'id' : 1,
+    'image' : 'https://placeimg.com/64/64/1',
+    'name' : '장재용',
+    'birthday' : '93XXXX',
+    'gender' : '남자',
+    'job' : '회사원',
+  },
+  {
+    'id' : 2,
+    'image' : 'https://placeimg.com/64/64/2',
+    'name' : '김철수',
+    'birthday' : '91XXXX',
+    'gender' : '남자',
+    'job' : '프리랜서',
+  },
+  {
+    'id' : 3,
+    'image' : 'https://placeimg.com/64/64/3',
+    'name' : '이영희',
+    'birthday' : '92XXXX',
+    'gender' : '여자',
+    'job' : '대학생',
+  },
+]
 
 class App extends Component {
   render(){
     return (
-      <div className="my-background">
-        <img src={logo} lat="logo"/>
-        <h2>Let's develop management system!</h2>  
+      <div>
+        {
+          customers.map(c => {
+            return (
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            );
+          })
+        }
       </div>
     );
   }
