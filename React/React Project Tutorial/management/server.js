b98cffd5be04f824f304ece149edc08e45001e70
Jang Jae Yong <jangjs2412@gmail.com>
2020-05-22 09:50:00 +0900

    Copy server.js

--- React/React Project Tutorial/management/server.js
+++ React/React Project Tutorial/management/server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!'});
-});
+app.get('/api/customers', (req,res) => {
+    res.send([
+        {
+            'id' : 1,
+            'image' : 'https://placeimg.com/64/64/1',
+            'name' : '장재용',
+            'birthday' : '93XXXX',
+            'gender' : '남자',
+            'job' : '회사원',
+        },
+        {
+            'id' : 2,
+            'image' : 'https://placeimg.com/64/64/2',
+            'name' : '김철수',
+            'birthday' : '91XXXX',
+            'gender' : '남자',
+            'job' : '프리랜서',
+        },
+        {
+            'id' : 3,
+            'image' : 'https://placeimg.com/64/64/3',
+            'name' : '이영희',
+            'birthday' : '92XXXX',
+            'gender' : '여자',
+            'job' : '대학생',
+        },
+    ])
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
